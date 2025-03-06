f258af0343fa74c67e66477693b7918601782442
Van Nguyen <xinchaonnv@gmail.com>
2025-03-06 15:55:44 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function draw() {
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
       const pixelIndex = (video.width -i + 1 + (j * video.width)) * 4;
-      const r = video.pixels[pixelIndex + 1];
+      const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
